1b517f3aabb0052194642ef4c793c983d8ff5e2f
kirillsnovikov <kirillsnovikov@gmail.com>
2020-03-04 02:03:55 +0300

    add routes

--- src/server/index.js
+++ src/server/index.js
@@ -1,32 +1,33 @@
 import React from 'react';
 import ReactDOMServer from 'react-dom/server';
+import { StaticRouter } from 'react-router-dom';
 import express from 'express';
 import App from '../shared/App';
 import paths from '../../build/paths';
 import fs from 'fs';
 
 const PORT = process.env.PORT || 3000;
-
 const { filenameHtml, dist } = paths;
 
 const app = express();
 app.use('/assets', express.static(dist));
-// app.use(express.static('./dist/client'));
-app.get('*', (req, res) => {
+app.get('/', (req, res) => {
   console.log('rrr');
+  const context = {};
+  const app = ReactDOMServer.renderToString(
+    <StaticRouter location={req.url} context={context}>
+      <App />
+    </StaticRouter>
+  );
+
+  console.log(context);
   fs.readFile(filenameHtml, 'utf-8', (err, data) => {
     if (err) {
       console.error(err);
       return res.status('404').send('Something went wrong');
     }
     return res.send(
-      data.replace(
-        '<div id="root"></div>',
-        `<div id="root">${ReactDOMServer.renderToString(
-          <App />
-        )}_${Math.random()}</div>`
-        // `<div id="root">123123</div>`
-      )
+      data.replace('<div id="root"></div>', `<div id="root">${app}</div>`)
     );
   });
 });
